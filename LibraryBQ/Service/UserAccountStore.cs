9115b1454b1f8fbfc3d1db3139525b2839cd0183
hajun05 <hajun0325175@gmail.com>
2025-06-30 17:24:35 +0900

    fate: UserAccount(로그인한 사용자 계정) 정보 처리 기본 구조 1. 여러 ViewModel이 공유하는 로그인 계정 정보를 Service 폴더-UserAccountStore 파일로 분리. 2. UserAccountStore에 싱글톤 패턴 적용. 3. UserAccountViewModel에서 UserAccountStore 인스턴스 생성, 해제(로그인, 로그아웃) 커멘드 베이스 구현.

---
+++ LibraryBQ/Service/UserAccountStore.cs
@@ -0,0 +1,45 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LibraryBQ.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBQ.Service
+{
+    // 현재 로그인한 사용자 정보 저장 서비스
+    internal partial class UserAccountStore : ObservableObject
+    {
+        // 싱글톤 패턴 적용
+        static UserAccountStore userAccount;
+        private UserAccountStore(User _loginUser)
+        {
+            CurrentUserAccount = _loginUser;
+        }
+        public static UserAccountStore Instance(User _loginUser)
+        {
+            if (userAccount == null)
+            {
+                userAccount = new UserAccountStore(_loginUser);
+            }
+            return userAccount;
+        }
+        public static void DetachInstance()
+        {
+            if (userAccount != null)
+            {
+                userAccount = null;
+            }
+        }
+
+        private User? _currentUserAccount;
+
+        public User? CurrentUserAccount
+        {
+            get { return _currentUserAccount; }
+            set => SetProperty(ref _currentUserAccount, value);
+        }
+    }
+}
---
+++ LibraryBQ/ViewModel/UserAccountViewModel.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LibraryBQ.Model;
+using LibraryBQ.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LibraryBQ.ViewModel
+{
+    // 로그인 처리 ViewModel
+    internal partial class UserAccountViewModel : ObservableObject
+    {
+        private UserAccountStore accountStore;
+
+        public UserAccountStore AccountStore
+        {
+            get => accountStore;
+            set => SetProperty(ref accountStore, value);
+        }
+
+        [RelayCommand] private void Login(string userNo, string password)
+        {
+            if (userNo.Trim() == "" || password.Trim() == "")
+            {
+                MessageBox.Show("필수 정보들을 입력해 주십시오.");
+            }
+            else
+            {
+                using (var db = new LibraryBQContext())
+                {
+                    User? LoginUser = db.Users.FirstOrDefault(x => x.UserNo == userNo && x.Password.Equals(password));
+                    if (LoginUser == null)
+                    {
+                        MessageBox.Show($"아이디 혹은 비밀번호가 틀립니다.\r\n다시 한번 확인해 주십시오.");
+                    }
+                    else
+                    {
+                        AccountStore = UserAccountStore.Instance(LoginUser);
+                    }
+                }
+            }
+        }
+
+        [RelayCommand] private void Logout()
+        {
+            UserAccountStore.DetachInstance();
+        }
+    }
+}
