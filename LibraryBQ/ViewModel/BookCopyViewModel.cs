ad74fc4dfd20e4ec1c21e403eaa86fafa521c19f
hajun05 <hajun0325175@gmail.com>
2025-06-30 17:17:09 +0900

    feat: 기본 ViewModel 폴더 구조 구현

---
+++ LibraryBQ/ViewModel/BookCopyViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LibraryBQ.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBQ.ViewModel
+{
+    internal class BookCopyViewModel
+    {
+    }
+}
