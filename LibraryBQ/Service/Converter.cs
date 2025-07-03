d32a93bd4f4f2e17f1d0381ee8a082e510a514c3
hajun05 <hajun0325175@gmail.com>
2025-07-04 00:44:46 +0900

    feat(Converter): Bool 프로퍼티를 Visibility에 바인딩하기 위한 변환기 구현

---
+++ LibraryBQ/Service/Converter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LibraryBQ.Service
+{
+    // Bool 프로퍼티를 Visibility에 바인딩하기 위한 변환기(true -> visible, false -> collapsed)
+    public class BooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (Visibility)value == Visibility.Visible;
+        }
+    }
+
+    // Bool 프로퍼티를 Visibility에 바인딩하기 위한 역변환기(false -> visible, true -> collapsed)
+    public class ReverseBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (Visibility)value == Visibility.Visible;
+        }
+    }
+}
