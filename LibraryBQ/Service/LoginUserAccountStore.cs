1a289014b281dc2408763bc9b04f9a7f90dedfe6
hajun05 <hajun0325175@gmail.com>
2025-07-04 00:48:22 +0900

    feat(LoginUserAccountStore): 현재 로그인한 사용자 정보 저장 서비스 구현 1. 기존 UserAccountStore 클래스명을 보다 명확하게 변경. 2. 생성자에서 현재 로그인한 사용자 객체를 인수로 받아 로그인한 사용자 프로퍼티(CurrentLoginUserAccount)를 초기화 하도록 변경. 3. 로그아웃시 기존 LoginUserAccountStore 싱글톤 인스턴스 자체를 null로 해제하던것을 로그인한 사용자 인스턴스를 해제하도록 변경.

---
+++ LibraryBQ/Service/LoginUserAccountStore.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using LibraryBQ.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBQ.Service
+{
+    // 현재 로그인한 사용자 정보 저장 서비스
+    public partial class LoginUserAccountStore : ObservableObject
+    {
+        // 필드와 프로퍼티 -------------------------------------------
+        private User? _currentUserAccount;
+
+        public User? CurrentLoginUserAccount
+        {
+            get { return _currentUserAccount; }
+            set => SetProperty(ref _currentUserAccount, value);
+        }
+
+        // 생성자(싱글톤 패턴 적용) ------------------------------------
+        private static LoginUserAccountStore userAccount;
+        private LoginUserAccountStore(User loginUser) 
+        {
+            CurrentLoginUserAccount = loginUser;
+        }
+        public static LoginUserAccountStore Instance(User loginUser)
+        {
+            if (userAccount == null)
+            {
+                userAccount = new LoginUserAccountStore(loginUser);
+            }
+            return userAccount;
+        }
+
+        // 메소드 ----------------------------------------------------
+        public static void DetachInstance()
+        {
+            if (userAccount != null)
+            {
+                userAccount = null;
+            }
+        }
+    }
+}
