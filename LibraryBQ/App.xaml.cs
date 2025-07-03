71875be97b39af6c6773838a4fe967856cefdb9f
hajun05 <hajun0325175@gmail.com>
2025-07-03 12:12:14 +0900

    feat(App): DI 컨테이너에 서비스 및 클라이언트(Main, Home, Login) 등록

--- LibraryBQ/App.xaml.cs
+++ LibraryBQ/App.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using LibraryBQ.Model;
+using LibraryBQ.View;
+using LibraryBQ.ViewModel;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
+using System;
 using System.Configuration;
 using System.Data;
 using System.Windows;
@@ -17,7 +20,7 @@ namespace LibraryBQ
         // WPF는 기본적으로 DI로 구성 객체가 미등록, 직접 구현 필요
         public static IConfiguration Config { get; private set; }
 
-        // DI 컨테이너 생성
+        // DI 컨테이너 프로퍼티 선언
         public static IServiceProvider ServiceProvider { get; private set; }
 
         public App()
@@ -32,10 +35,25 @@ namespace LibraryBQ
             services.AddDbContextPool<LibraryBQContext>(options =>
                 options.UseSqlServer(App.Config.GetConnectionString("MSSQLConnection")));
 
-
+            // 서비스 컬렉션에 서비스(ViewModel) 및 클라이언트(View) 등록.
+            // DI 컨테이너를 이용한 View와 ViewModel 연결. View 생성 시 ViewModel 자동 주입 및 바인딩
+            services.AddTransient<MainWindowViewModel>();
+            services.AddTransient<HomeViewModel>();
+            services.AddTransient<LoginViewModel>();
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<HomeView>();
+            services.AddSingleton<LoginView>();
 
             // DI 컨테이너 초기화
             ServiceProvider = services.BuildServiceProvider();
+        }
+
+        // StartupUri 속성 대체 메소드. WPF가 자동으로 지정된 윈도우를 생성 및 표시. 별도 호출 불필요
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
         }
     }
 
