4565a15ffe8905531497280e0e535f0233bbcf0e
hajun05 <hajun0325175@gmail.com>
2025-07-03 14:39:38 +0900

    feat(MainWindowView, ViewModel): MainWindowView - MainWindowViewModel 바인딩 1. MainWindowViewModel 기본 구조 구현(CurrentViewModel). MainView의 하단 구역 Frame에 바인딩할 ViewModel 프로퍼티. 2. MainWindowView의 초기 화면으로 HomeViewModel 설정. 3. MainWindowView에 ContentControl + DataTemplate 구조 구현. ViewModel 타입에 따라 자동으로 View를 매핑.

--- LibraryBQ/View/MainWindow.xaml.cs
+++ LibraryBQ/View/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LibraryBQ.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,10 @@ namespace LibraryBQ.View
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public MainWindow(MainWindowViewModel viewModel)
         {
             InitializeComponent();
+            DataContext = viewModel;
         }
 
         private void Closebtn_Click(object sender, RoutedEventArgs e)
--- LibraryBQ/ViewModel/MainWindowViewModel.cs
+++ LibraryBQ/ViewModel/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using LibraryBQ.Model;
+using LibraryBQ.Service;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,7 +10,32 @@ using System.Threading.Tasks;
 
 namespace LibraryBQ.ViewModel
 {
-    class MainWindowViewModel : ObservableObject
+    public partial class MainWindowViewModel : ObservableObject
     {
+        // 필드와 프로퍼티 --------------------------------------
+        private ObservableObject _currentViewModel;
+        private HomeViewModel _homeViewModel;
+
+        public ObservableObject CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set => SetProperty(ref _currentViewModel, value);
+        }
+
+        // 생성자 ----------------------------------------------
+        public MainWindowViewModel(HomeViewModel homeViewModel)
+        {
+            _homeViewModel = homeViewModel;
+
+            // 초기화면
+            CurrentViewModel = _homeViewModel;
+        }
+
+        // 커멘드 ----------------------------------------------
+        [RelayCommand] private void HomebtnClick() // 홈버튼 클릭 커멘드
+        {
+            if (CurrentViewModel != _homeViewModel)
+                CurrentViewModel = _homeViewModel;
+        }
     }
 }
