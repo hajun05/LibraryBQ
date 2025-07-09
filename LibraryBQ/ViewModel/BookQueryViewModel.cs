6fc3aa054f94da05bb20581b85f19f3e0ed2e4c4
hajun05 <hajun0325175@gmail.com>
2025-07-09 16:29:59 +0900

    feat(BookQuery): 조회한 도서 목록 프로퍼티 개선 1. List<Book> -> ObservableCollection<Book> 변경. 바인딩 오류 예방. 2. Books -> QueriedBooks 명칭 변경.

--- LibraryBQ/ViewModel/BookQueryViewModel.cs
+++ LibraryBQ/ViewModel/BookQueryViewModel.cs
@@ -1,10 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
-using LibraryBQ.Service;
 using LibraryBQ.Model;
+using LibraryBQ.Service;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,17 +17,17 @@ namespace LibraryBQ.ViewModel
     {
         // 필드와 프로퍼티 -----------------------------------------------------------
         private string _inputQueryStr;
-        private List<Book> _books;
+        private ObservableCollection<Book> _queriedBooks;
         private Book? _selectedBook;
         public string InputQueryStr
         {
             get => _inputQueryStr;
             set => SetProperty(ref _inputQueryStr, value);
         }
-        public List<Book> Books
+        public ObservableCollection<Book> QueriedBooks
         {
-            get { return _books; }
-            set => SetProperty(ref _books, value);
+            get { return _queriedBooks; }
+            set => SetProperty(ref _queriedBooks, value);
         }
         public Book? SelectedBook
         {
@@ -34,28 +35,34 @@ namespace LibraryBQ.ViewModel
             set => SetProperty(ref _selectedBook, value);
         }
 
+        // 생성자 ------------------------------------------------------------------
         public BookQueryViewModel()
         {
+            _queriedBooks = new ObservableCollection<Book>();
             WeakReferenceMessenger.Default.Register<CommandMessage>(this, (r, m) => BookQueryCommand.Execute(null));
         }
 
         // 커멘드 -------------------------------------------------------------------
         [RelayCommand] private void BookQuery()
         {
-            if (Books != null)
-                Books.Clear();
+            if (_queriedBooks != null)
+                _queriedBooks.Clear();
             using (LibraryBQContext db = new LibraryBQContext())
             {
+                List<Book> result = new List<Book>();
                 if (InputQueryStr.Trim() != "")
                 {
-                    Books = db.Books.Where(x => x.Title.Contains(InputQueryStr)).ToList();
-                    Books.AddRange(db.Books.Where(x => x.Author.Contains(InputQueryStr)).ToList());
+                    result = db.Books.Where(x => x.Title.Contains(InputQueryStr)).ToList();
+                    result.AddRange(db.Books.Where(x => x.Author.Contains(InputQueryStr)).ToList());
                 }
                 else
                 {
-                    Books = db.Books.ToList();
-                    Books.AddRange(db.Books.ToList());
+                    result = db.Books.ToList();
+                    result.AddRange(db.Books.ToList());
                 }
+
+                foreach (Book book in result)
+                    _queriedBooks.Add(book);
             }
         }
     }
