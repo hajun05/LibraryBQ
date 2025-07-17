5339b32228038659ddfb8ff2ac233b53976da944
hajun05 <hajun0325175@gmail.com>
2025-07-17 14:20:33 +0900

    feat(BookCopy): 도서 부수 정보 조회 UI 디자인 구현 1. 부수정보 출력용 BookCopyDetail DTO 구현. 2. 부수정보 쿼리 메소드 구현.

---
+++ LibraryBQ/Service/BookCopyDetail.cs
@@ -0,0 +1,45 @@
+﻿using LibraryBQ.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBQ.Service
+{
+    public partial class BookCopyDetail
+    {
+        // 도서 관련 프로퍼티 ---------------------------------------------
+        public int BookCopyId { get; set; }
+        public int BookId { get; set; }
+        public string ClassificationNumber { get; set; }
+
+        // 현재 상태 관련 프로퍼티 -----------------------------------------
+        public int CurrentLoanStatusId { get; set; }
+        public string CurrentStatusName { get; set; } = null!;
+
+        // 대출 상태 관련 프로퍼티 -----------------------------------------
+        public DateOnly? CurrentLoanDueDate { get; set; }
+        public int? CurrentLoanUserId { get; set; }
+
+        // 예약 상태 관련 프로퍼티 -----------------------------------------
+        public List<ReservationHistory>? CurrentReservations { get; set; }
+
+        // 메소드 --------------------------------------------------------
+        public static string ConvertLoanStatus(int _currentLoanStatusId)
+        {
+            string answer = string.Empty;
+            switch (_currentLoanStatusId)
+            {
+                case 1:
+                    answer = "대출가능"; break;
+                case 2:
+                    answer = "대출중"; break;
+                case 3:
+                    answer = "예약중"; break;
+            }
+            return answer;
+        }
+    }
+}
--- LibraryBQ/ViewModel/BookCopyViewModel.cs
+++ LibraryBQ/ViewModel/BookCopyViewModel.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using Microsoft.EntityFrameworkCore;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using LibraryBQ.Service;
 using LibraryBQ.Model;
@@ -7,23 +8,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace LibraryBQ.ViewModel
 {
     public partial class BookCopyViewModel : ObservableObject
     {
-        // 필드 및 프로퍼티
-        private Book _book;
-        public Book Book
+        // 필드 및 프로퍼티 -----------------------------------------------------------
+        private Book _selectedBook;
+        private ObservableCollection<BookCopyDetail> _bookCopies;
+        public Book SelectedBook
         {
-            get { return _book; }
-            set => SetProperty(ref _book, value);
+            get { return _selectedBook; }
+            set => SetProperty(ref _selectedBook, value);
+        }
+        public ObservableCollection<BookCopyDetail> BookCopies
+        {
+            get { return _bookCopies; }
+            set => SetProperty(ref _bookCopies, value);
         }
 
         // 생성자 --------------------------------------------------------------------
         public BookCopyViewModel(Book selectedBook)
         {
-            _book = selectedBook;
+            _selectedBook = selectedBook;
+            _bookCopies = new ObservableCollection<BookCopyDetail>();
+            BookCopiesQuery();
+        }
+
+        // 메소드 --------------------------------------------------------------------
+        private void BookCopiesQuery()
+        {
+            using (LibraryBQContext db = new LibraryBQContext())
+            {
+                List<BookCopyDetail> queriedBookCopies = new List<BookCopyDetail>();
+                queriedBookCopies = db.BookCopies.Include(x => x.Book).Include(x => x.LoanStatus)
+                    .Include(x => x.LoanHistories).Where(x => x.BookId == SelectedBook.Id)
+                    .Select(x => new BookCopyDetail()
+                    {
+                        BookCopyId = x.Id,
+                        BookId = x.BookId,
+                        ClassificationNumber = String.Format($"{x.BookId}-{x.Id}"),
+                        CurrentLoanStatusId = x.LoanStatusId,
+                        CurrentStatusName = BookCopyDetail.ConvertLoanStatus(x.LoanStatusId),
+                        CurrentLoanDueDate = (x.LoanStatusId == 2) ? x.LoanHistories.OrderByDescending(lh => lh.Id).FirstOrDefault().LoanDueDate : null,
+                        CurrentLoanUserId = (x.LoanStatusId == 2) ? x.LoanHistories.OrderByDescending(lh => lh.Id).FirstOrDefault().UserId : -1,
+                        CurrentReservations = x.ReservationHistories.ToList(),
+                    }).ToList();
+
+                foreach(BookCopyDetail bookCopy in queriedBookCopies)
+                    _bookCopies.Add(bookCopy);
+            }
         }
     }
 }
